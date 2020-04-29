fe9d6a7e2edf94ccb4db92534444d08472f96ee3
SaiKrishna <saikrishna31120@gmail.com>
2020-04-29 20:41:31 +0530

    Works somehow. its dirty and lacks interface right now

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
-import cv2
-import sys
+# this is the finall app that will load the trained model and take input from
+# webcam. work on it after the model is done
+# load and evaluate a saved model
+import numpy as np
+from numpy import loadtxt
+from keras.models import load_model
+from keras.models import model_from_json
+from keras.preprocessing import image
 
-imagePath = sys.argv[1]
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+loaded_model.summary()
 
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.3,
-    minNeighbors=3,
-    minSize=(30, 30)
-)
-
-print("[INFO] Found {0} Faces.".format(len(faces)))
-
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    roi_color = image[y:y + h, x:x + w]
-    print("[INFO] Object found. Saving locally.")
-    cv2.imwrite(str(w) + str(h) + '_faces.jpg', roi_color)
-
-status = cv2.imwrite('faces_detected.jpg', image)
-print("[INFO] Image faces_detected.jpg written to filesystem: ", status)
+test_img=[]
+img = image.load_img('test/s.png', 
+                         target_size=(48,48,1),
+                         color_mode="grayscale")
+  # convert image to an numpy array
+img =  image.img_to_array(img)
+# to keep the values from 0 to 1
+img = img/255
+test_img.append(img)
+test = np.array(test_img)
+prediction = loaded_model.predict_classes(test)
+emotions=['anger','contempt','disgust','fear','happy','sadness','surprise']
+print(emotions[prediction[0]])
