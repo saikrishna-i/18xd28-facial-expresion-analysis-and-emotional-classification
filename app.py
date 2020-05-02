9b9c83871a1fb3cca228be17b045cba1d60b1dcd
SaiKrishna <saikrishna31120@gmail.com>
2020-05-02 13:27:57 +0530

    tested new model

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ from keras.models import load_model
 from keras.models import model_from_json
 from keras.preprocessing import image
 
-json_file = open('model.json', 'r')
+json_file = open('model/model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
-loaded_model.load_weights("model.h5")
+loaded_model.load_weights("model/model.h5")
 print("Loaded model from disk")
 loaded_model.summary()
 
